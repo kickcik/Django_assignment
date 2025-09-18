0f43574fcfee40acdd0367848df0c73422e9894a
kickcik <4778009@gmail.com>
2025-09-18 17:55:44 +0900

    set static

--- config/settings.py
+++ config/settings.py
@@ -121,10 +121,14 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (css, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
