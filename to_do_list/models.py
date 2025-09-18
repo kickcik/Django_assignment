3247c5f5aeaede943b20558c19474b6a1dfa9eca
kickcik <4778009@gmail.com>
2025-09-18 18:00:57 +0900

    add author field

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 from django.db import models
@@ -9,6 +10,7 @@ class ToDoList(models.Model):
     is_complete = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = '투두'
