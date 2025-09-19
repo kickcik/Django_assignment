0723a7788c1ea77d5880563ceeae0bc4cf89402a
kickcik <4778009@gmail.com>
2025-09-19 18:58:03 +0900

    add def absolute url

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -2,6 +2,9 @@ from django.contrib.auth.models import User
 from django.db import models
 
 from django.db import models
+from django.urls import reverse
+
+
 class ToDoList(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
@@ -11,6 +14,10 @@ class ToDoList(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+        return reverse('todo:info', kwargs={'pk': self.pk})  # 수정
+
 
     class Meta:
         verbose_name = '투두'
