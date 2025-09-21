fca5859df10d3bc74cb5d8f28d045f36f9349fb6
kickcik <4778009@gmail.com>
2025-09-21 22:08:07 +0900

    add new form for comment

--- to_do_list/forms.py
+++ to_do_list/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from to_do_list.models import ToDoList
+from to_do_list.models import ToDoList, Comment
 
 
 class TodoForm(forms.ModelForm):
@@ -12,4 +12,16 @@ class TodoForm(forms.ModelForm):
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
             'start_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'end_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content', )
+        # 폼 필드가 렌더링될 때 사용할 HTML 위젯을 정의
+        widgets = {
+            'content': forms.Textarea(attrs={'placeholder': '댓글을 입력하세요...', 'rows': 3})
+        }
+        labels = {
+            'content': '댓글'
         }
