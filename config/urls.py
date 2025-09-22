f6b75145701cd8234d4dbe228463f96bafac89d3
kickcik <4778009@gmail.com>
2025-09-23 01:27:35 +0900

    fixed image crack

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from to_do_list import views as todo_list_views
@@ -36,3 +38,6 @@ urlpatterns = [
     path('summnernote/', include('django_summernote.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
